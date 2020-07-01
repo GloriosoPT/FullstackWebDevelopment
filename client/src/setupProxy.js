e68a87a3dda6b71f25c8d7ea36816a12bd8bac7f
Fabio Santos <fabiosantos@c02zl0ddlvdl.mynet>
2020-07-01 08:53:01 +0100

    change proxy to api instead of api*

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+ 
 module.exports = function(app) {
   app.use(
-    ['/api/*','/login'],
+    ['/api', '/login'],
     createProxyMiddleware({
       target: 'http://localhost:5000'
     })
